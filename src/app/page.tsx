1ac0c80d542f33f325d35dcb4bcaf94017c6f285
Yosuke Hiraoka <yosuke.h@chot-inc.com>
2023-10-13 17:49:46 +0900

    配信元がわかるように変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react";
 
 const Page: FC = async () => {
-  return <h1 className="text-xl font-bold">TOP PAGE</h1>;
+  return (
+    <h1 className="text-xl font-bold">
+      {process.env.STATIC_EXPORT ? "S3 Provided" : "Vercel Provided"}
+    </h1>
+  );
 };
 
 export default Page;
